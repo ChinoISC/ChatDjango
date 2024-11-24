dd0fa380139232258e4518c92224d728191372a7
ChinoISC <oscar010394@gmail.com>
2024-11-24 10:12:01 -0600

    Host Add

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m*qhjuf=f*sd+y$k)t-w!y8p2pcqhk_%*imzf$_yow_27ffj#v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://chatdjango-7azm.onrender.com']
 
 
 # Application definition
