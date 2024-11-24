4ba3e57d41bd6f01d0366b4ddc4640445266aea3
ChinoISC <oscar010394@gmail.com>
2024-11-24 11:03:16 -0600

    Cambios

--- api/views.py
+++ api/views.py
@@ -5,12 +5,10 @@ from telegram import Update, Bot
 from telegram.ext import ApplicationBuilder
 import json
 
-# Token del bot de Telegram
 TOKEN = '7726637693:AAFGRFI1fhRqmBucmztsVvaidi1IYp1gSRs'
 
-# Crea la instancia de la aplicación Telegram
+# Responder a los mensajes recibidos
 async def process_update(update: Update, bot: Bot):
-    # Aquí puedes manejar la lógica de tu bot, como responder a mensajes
     if update.message:
         text = update.message.text
         await update.message.reply_text(f"Recibí tu mensaje: {text}")
@@ -19,16 +17,11 @@ async def process_update(update: Update, bot: Bot):
 @csrf_exempt
 def webhook(request):
     if request.method == 'POST':
-        # Los datos de la actualización de Telegram llegan en el cuerpo de la solicitud
         update_data = json.loads(request.body.decode('utf-8'))
-        
-        # Crear el bot de Telegram
         bot = Bot(TOKEN)
-
-        # Crear el objeto Update a partir de los datos recibidos
         update = Update.de_json(update_data, bot)
 
-        # Procesa el mensaje recibido
+        # Procesar el mensaje recibido y enviar una respuesta
         application = ApplicationBuilder().token(TOKEN).build()
         application.dispatcher.process_update(update)
 
