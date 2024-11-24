21ca41eabca77dae3ed008f133820f0c6bebee5f
ChinoISC <oscar010394@gmail.com>
2024-11-24 11:42:33 -0600

    Cambios

--- api/telegram_bot.py
+++ api/telegram_bot.py
@@ -1,46 +1,42 @@
 
-import logging
-import json
 from django.http import JsonResponse
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from django.conf import settings
+import json
+import logging
 
-# Habilitar los registros de logging para Telegram
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-async def start(update: Update, context):
-    await update.message.reply_text('¡Hola! Soy tu bot en Django.')
-
-async def echo(update: Update, context):
-    # Repetir el mensaje de vuelta al usuario
-    await update.message.reply_text(update.message.text)
-
-async def process_update(request):
+async def telegram_webhook(request):
     try:
-        # Decodificar correctamente el cuerpo del request
+        # Decodificar adecuadamente el cuerpo del request
         json_str = request.body.decode('UTF-8')  # Convertir de bytes a string
         update = Update.de_json(json.loads(json_str))  # Convertir string a objeto JSON
 
-        # Crear la aplicación en vez de ‘Updater’ y configurar handlers
+        # Inicializar la aplicación
         application = Application.builder().token(settings.TELEGRAM_BOT_TOKEN).build()
 
-        # Añadir manejadores
+        # Añadir los manejadores de comandos y mensajes
         application.add_handler(CommandHandler("start", start))
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
         # Procesar la actualización
         await application.update_queue.put(update)
 
-        # Procesar el evento de inicio de aplicación asincrónicamente
+        # Inicializar y procesar aplicaciones correctas asincrónicamente
         await application.initialize()
         await application.start()
         await application.stop()
 
-        print(json_str)
+        # Enviar una respuesta JSON
         return JsonResponse({'status': 'ok'})
     except Exception as e:
         logger.error(f"Error al procesar la actualización: {e}")
         return JsonResponse({'status': 'error', 'message': str(e)})
+
+async def start(update: Update, context):
+    await update.message.reply_text('¡Hola! Soy tu bot en Django.')
+
+async def echo(update: Update, context):
+    await update.message.reply_text(update.message.text)
